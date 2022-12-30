20a3cdcf7a916136989f8ce29685ae03e5a0246f
Callee84 <carl.g.holm@gmail.com>
2022-12-30 15:25:56 +0000

    Add style to navbar and links for navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom'
+import "./api/axiosDefaults";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route exact path='/' render={() => <h1>Home Page</h1>} />
           <Route exact path='/feed' render={() => <h1>Feed</h1>} />
+          <Route exact path='/wine' render={() => <h1>Wine</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+          <Route exact path='/signup' render={() => <h1>Sign Out</h1>} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,28 +2,50 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.jpg'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand><img src={logo} alt="logo" height="60" />
         </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto ">
-            <Nav.Link href="#home">
+            <NavLink
+            exact
+            className={styles.NavLink}
+            activeClassName={styles.Active} 
+            to="/">
             <i className="fas fa-home"/>Home
-            </Nav.Link>
-            <Nav.Link href="#link">
-            <i class="fa-solid fa-wine-bottle" />Feed
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active} 
+            to="/feed">
+            <i class="fa-solid fa-comment-dots" />Feed
+            </NavLink>
+            <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active} 
+            to="/wine">
+            <i class="fa-solid fa-wine-bottle" />Wine
+            </NavLink>
+            <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active} 
+            to="/signin">
               <i className="fas fa-sign-in-alt"/>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active} 
+            to="/signup">
               <i className="fas fa-user-plus"/>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
