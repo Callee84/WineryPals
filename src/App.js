0935139b0b004afd6e797138473db620907ea100
Callee84 <carl.g.holm@gmail.com>
2023-01-03 11:49:42 +0000

    Add title, textfield and upload image for create post

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom'
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm';
 import LogInForm from './pages/auth/LogInForm';
+import CreatePost from './pages/posts/CreatePost';
+
 
 
 function App() {
@@ -15,10 +17,11 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path='/' render={() => <h1>Home Page</h1>} />
-          <Route exact path='/feed' render={() => <h1>Feed</h1>} />
-          <Route exact path='/wine' render={() => <h1>Wine</h1>} />
           <Route exact path='/signin' render={() => <LogInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
+          <Route exact path='/posts/create' render={() => <CreatePost />} />
+          <Route exact path='/wine' render={() => <h1>Wine</h1>} />
+          <Route exact path='/feed' render={() => <h1>Feed</h1>} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
